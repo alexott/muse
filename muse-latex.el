d7add74db9094adc8fa9796c17ccd86476fc087f
Michael Olson <mwolson@gnu.org>
2005-04-14 23:46:47 +0000

    Merge contents of muse-latexcjk.el into muse-latex.el.
    2005-04-14  Michael Olson  <mwolson@gnu.org>
    
        * muse-latex.el (muse-latexcjk-header, muse-latexcjk-footer)
        (muse-latexcjk-encoding-map, muse-latexcjk-encoding-default)
        (muse-latexcjk-encoding, muse-latexcjk-transform-content-type):
        Merge in from muse-latexcjk.el.
    
        * muse-latexcjk.el: Removed, functionality merged with
        muse-latex.el
    git-archimport-id: mwolson@gnu.org--2005/muse--main--1.0--patch-25

--- muse-latex.el
+++ muse-latex.el
@@ -47,6 +47,36 @@
   "Footer used for publishing LaTeX files."
   :type '(choice string file)
   :group 'muse-latex)
+(defcustom muse-latexcjk-header
+  "\\documentclass{article}
+
+\\usepackage{CJK}
+\\usepackage{indentfirst}
+\\usepackage[CJKbookmarks=true]{hyperref}
+\\usepackage[pdftex]{graphicx}
+
+\\begin{document}
+\\begin{CJK*}<lisp>(muse-latexcjk-encoding)</lisp>
+
+\\title{<lisp>(muse-publishing-directive \"title\")</lisp>}
+\\author{<lisp>(muse-publishing-directive \"author\")</lisp>}
+\\date{<lisp>(muse-publishing-directive \"date\")</lisp>}
+
+\\maketitle
+
+<lisp>(and muse-publish-generate-contents
+	   \"\\\\tableofcontents\n\\\\newpage\")</lisp>\n\n"
+  "Header used for publishing LaTeX files(CJK)."
+  :type '(choice string file)
+  :group 'muse-latex)
+
+(defcustom muse-latexcjk-footer 
+  "
+\\end{CJK*}
+\\end{document}"
+  "Footer used for publishing LaTeX files(CJK)."
+  :type '(choice string file)
+  :group 'muse-latex)
 
 (defcustom muse-latex-markup-regexps
   `(;; numeric ranges
@@ -139,6 +169,35 @@ differs little between the various styles."
   :type '(alist :key-type symbol :value-type string)
   :group 'muse-latex)
 
+(defcustom muse-latexcjk-encoding-map
+  '((utf-8		. "{UTF8}{song}")
+    (japanese-iso-8bit	. "[dnp]{JIS}{min}")
+    (chinese-big5	. "{Bg5}{bsmi}")
+    (mule-utf-8         . "{UTF8}{song}")
+    (chinese-iso-8bit   . "{GB}{song}")
+    (chinese-gbk        . "{GBK}{song}"))
+  "An alist mapping emacs coding systems to appropriate CJK codings.
+  Use the base name of the coding system (ie, without the -unix)"
+  :type '(alist :key-type coding-system :value-type string)
+  :group 'muse-latex)
+
+(defcustom muse-latexcjk-encoding-default "{GB}{song}"
+  "The default emacs coding  use if no special characters are found"
+  :type 'string
+  :group 'muse-latex)
+
+(defun muse-latexcjk-encoding ()
+  (muse-latexcjk-transform-content-type buffer-file-coding-system))
+
+(defun muse-latexcjk-transform-content-type (content-type)
+  "Using `muse-cjklatex-encoding-map', try and resolve an emacs coding
+system to an associated CJK coding system."
+  (let ((match (assoc (coding-system-base content-type)
+		      muse-latexcjk-encoding-map)))
+    (if match
+	(cdr match)
+      muse-latexcjk-encoding-default)))
+
 (defcustom muse-latex-markup-specials
   '((?\\ . "\\\\"))
   "A table of characters which must be represented specially."
@@ -202,6 +261,15 @@ differs little between the various styles."
 		     :browser   'find-file)
 
   (muse-derive-style "pdf" "latex"
+		     :final   'muse-latex-pdf-generate
+		     :browser 'muse-latex-pdf-browse-file
+		     :osuffix 'muse-latex-pdf-extension)
+
+  (muse-derive-style "latexcjk" "latex"
+		     :header    'muse-latexcjk-header
+		     :footer    'muse-latexcjk-footer)
+
+  (muse-derive-style "pdfcjk" "latexcjk"
 		     :final   'muse-latex-pdf-generate
 		     :browser 'muse-latex-pdf-browse-file
 		     :osuffix 'muse-latex-pdf-extension))
