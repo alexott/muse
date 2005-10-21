04c8aa90e95a287ea5eafce9618fb8b513cfedb7
Michael Olson <mwolson@gnu.org>
2005-10-21 14:39:50 +0000

    Allow anchors to occur near (rather than at) the beginning of line.
    * lisp/muse-publish.el (muse-publish-markup-regexps): Allow non-word
      characters before anchor, like " - ".  Thanks to Phillip Lord for the
      suggestion.
    git-archimport-id: mwolson@gnu.org--2005/muse--main--1.0--patch-252

--- lisp/muse-publish.el
+++ lisp/muse-publish.el
@@ -97,7 +97,7 @@ be returned."
     (1350 "^;\\s-+\\(.+\\)" 0 comment)
 
     ;; define anchor points
-    (1400 "^#\\(\\S-+\\)\\s-*" 0 anchor)
+    (1400 "^\\W*#\\(\\S-+\\)\\s-*" 0 anchor)
 
     ;; prevent emphasis characters in explicit links from being marked
     (1500 muse-explicit-link-regexp 0 muse-publish-mark-noemphasis)
