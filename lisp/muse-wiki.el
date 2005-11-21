a6b89dea9b3d756a928c559b2bd1e35d55cc79a4
Michael Olson <mwolson@gnu.org>
2005-11-21 15:22:39 +0000

    muse-wiki: WikiWords can have consecutive capital letters.
    * lisp/muse-wiki.el (muse-wiki-wikiword-regexp): Use a looser definition
      of what constitutes a WikiWord.  Namely: capital letters can appear
      next to each other.
    git-archimport-id: mwolson@gnu.org--2005/muse--rel--3.02--patch-23

--- lisp/muse-wiki.el
+++ lisp/muse-wiki.el
@@ -51,8 +51,8 @@
 
 (defcustom muse-wiki-wikiword-regexp
   (concat "\\<\\(\\(?:[" muse-regexp-upper
-          "][" muse-regexp-lower "]+\\)\\(?:["
-          muse-regexp-upper "][" muse-regexp-lower "]+\\)+\\)\\>")
+          "]+[" muse-regexp-lower "]+\\)\\(?:["
+          muse-regexp-upper "]+[" muse-regexp-lower "]+\\)+\\)\\>")
   "Regexp used to match WikiWords."
   :type 'regexp
   :group 'muse-wiki
