1851969b4f28216307339e2c91e68255dfc21719
Michael Olson <mwolson@gnu.org>
2005-12-16 19:21:06 +0000

    Generate autoloads file.
    * Makefile (autoloads): New rule that calls "make autoloads" in lisp
      directory.
    
    * lisp/Makefile (EL): Avoid warning by removing muse-autoloads.el from
      list.
      (all): Generate autoloads.
      (autoloads): Make muse-autoloads.el.
      (muse-autoloads.el): Call `muse-generate-autoloads' from
      scripts/muse-build.el with arguments representing the directories to
      use when searching for autoloads.
      (realclean distclean fullclean): Remove autoloads.
    
    * scripts/muse-build.el (muse-generate-autoloads): Generate autoloads for
      Muse.  Deal with XEmacs and its quirky insistence on doing most of the
      work itself.  Expand all command line args so that the right
      directories are searched.  Hardcode muse-autoloads.el as the name of
      the generated autoloads file.  Use my custom header and footer, if
      possible.
    git-archimport-id: mwolson@gnu.org--2005/muse--main--1.0--patch-300

--- scripts/muse-build.el
+++ scripts/muse-build.el
@@ -35,3 +35,39 @@
                                    (muse-line-end-position)))
         (forward-line 1)))
     (kill-buffer (current-buffer))))
+
+(defun muse-generate-autoloads ()
+  (interactive)
+  (defvar autoload-package-name)
+  (defvar command-line-args-left)
+  (defvar generated-autoload-file)
+  (require 'autoload)
+  (setq generated-autoload-file (expand-file-name "muse-autoloads.el"))
+  (setq command-line-args-left (mapcar #'expand-file-name
+                                       command-line-args-left))
+  (if (featurep 'xemacs)
+      (progn
+          (setq autoload-package-name "muse")
+          (batch-update-autoloads))
+    (find-file generated-autoload-file)
+    (delete-region (point-min) (point-max))
+    (insert ";;; muse-autoloads.el --- autoloads for Muse
+;;
+;;; Code:
+")
+    (save-buffer 0)
+    (batch-update-autoloads)
+    (find-file generated-autoload-file)
+    (goto-char (point-max))
+    (insert ?\n)
+    (insert "(provide 'muse-autoloads)
+;;; muse-autoloads.el ends here
+;;
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:")
+    (insert ?\n)
+    (save-buffer 0)
+    (kill-buffer (current-buffer))))
