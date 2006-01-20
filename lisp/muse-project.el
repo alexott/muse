d0738eec89f2cbf42c458fe0d1c05ce2d8a25085
Michael Olson <mwolson@gnu.org>
2006-01-20 15:54:21 +0000

    Pick a better default if publishing with more than one style.
    * lisp/muse-project.el (muse-project-applicable-styles): Make sure that
      the list we return is in the right order.  That way, if more than one
      publishing rule matches a link, the first one is used, rather than the
      last.
    git-archimport-id: mwolson@gnu.org--2006/muse--main--1.0--patch-2

--- lisp/muse-project.el
+++ lisp/muse-project.el
@@ -528,7 +528,7 @@ The name of a project may be used for STYLES."
                            (not (string-match exclude-regexp file))))
                      (or (not (file-exists-p file))
                          (not (muse-project-private-p file))))
-            (add-to-list 'used-styles style))))
+            (add-to-list 'used-styles style t))))
       used-styles)))
 
 (defun muse-project-publish-file (file styles &optional force ignore-regexp)
