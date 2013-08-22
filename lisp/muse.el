e4c6b7b1c71da9995353319b4bc2b7c66461433f
Stefan Monnier <monnier@iro.umontreal.ca>
2013-08-22 14:10:36 -0400

    Change package description

--- lisp/muse.el
+++ lisp/muse.el
@@ -1,4 +1,4 @@
-;;; muse.el --- an authoring and publishing tool for Emacs
+;;; muse.el --- Authoring and publishing tool for Emacs
 
 ;; Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010
 ;;   Free Software Foundation, Inc.
